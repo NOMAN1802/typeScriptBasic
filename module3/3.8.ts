0d4b0e40bbd313a2459566f5ca21be625e64250a
NOMAN1802 <mustakimnoman82@gmail.com>
2024-05-07 16:18:13 +0600

    polymorphism

--- module3/3.8.ts
+++ module3/3.8.ts
@@ -26,14 +26,68 @@
                 } 
     }
 
-    const getSleepingHours = (params : Person) =>{
-        getSleep
+    const getSleepingHours = (param : Person) =>{
+        param.getSleep()
     }
 
 const person1 = new Person()
-const student1 = new Student()
-const developer1 = new Developer()
+const person2 = new Student()
+const person3 = new Developer()
 
+getSleepingHours(person1)
+getSleepingHours(person2)
+getSleepingHours(person3)
 
+class Shape {
+    getArea () : number {
+        return 0;
+    }
+}
+
+// Circle
+// pi * r * r
+class Circle extends Shape {
+    radius : number;
+
+    constructor(radius : number){
+    super()
+    this.radius =radius;
+    }
+    getArea(): number {
+        return Math.PI*this.radius *this.radius;
+}
+
+}
+// Rectangle
+// height * width
+class Rectangle extends Shape {
+    height : number;
+    width : number;
+
+    constructor( height: number,width : number){
+    super()
+    this.height =height,
+    this.width =width;
+    
+    }
+    
+    
+    getArea(): number {
+        return Math.PI*this.height *this.width;
+}
+
+}
+
+const getShapeArea = ( param: Shape) =>{
+    console.log(param.getArea());
+}
+
+const shape1 = new Shape()
+const shape2 = new Circle(10)
+const shape3 = new Rectangle(10, 20)
+
+getShapeArea(shape1)
+getShapeArea(shape2)
+getShapeArea(shape3)
     // 
 }
